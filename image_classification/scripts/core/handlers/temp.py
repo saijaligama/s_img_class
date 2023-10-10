f497347596157ae9eb649d5a9a193b6666ce6c59
saijaligama <saijaligama@icloud.com>
2023-10-09 19:08:53 -0500

    testing sessions baranch

--- image_classification/scripts/core/handlers/temp.py
+++ image_classification/scripts/core/handlers/temp.py
@@ -1,15 +1,11 @@
 import cv2
-import numpy as np
 import os
-# from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
 from tensorflow.keras.models import load_model
-
-
-# Import the required constant from your constants module
 from scripts.constants.global_constants import STATIC_FOLDER_IMAGES_SPLITIMAGES,CLASSIFIED_IMAGES,CNN_PREDICTION_MODEL
 
-class ClassifyImages:
 
+class ClassifyImages:
     def __init__(self, input_dir=STATIC_FOLDER_IMAGES_SPLITIMAGES, output_dir = CLASSIFIED_IMAGES, model_path=CNN_PREDICTION_MODEL):
         self.input_dir = input_dir
         self.output_dir = output_dir
@@ -17,7 +13,7 @@ class ClassifyImages:
         self.model = load_model(model_path, compile=False)
 
     def classify_images(self):
-        for j in range(8): # changer to input values
+        for j in range(8):
             for i in range(8):
                 img_path = os.path.join(self.input_dir, f"{j}_{i}.png")
                 img = cv2.imread(img_path)
@@ -31,16 +27,26 @@ class ClassifyImages:
                 print(img_path)
 
                 if prediction > 0.2: 
+                    # Create a green background
+                    background_color = (0, 255, 0)  # Green (BGR format)
+                else:
+                    # Create a red background
+                    background_color = (0, 0, 255)  # Red (BGR format)
+
+                # Create a new image with the specified background color
+                new_img = np.full_like(img, background_color)
+                # Combine the original image with the background
+                combined_img = cv2.addWeighted(img, 0.7, new_img, 0.3, 0)
+
+                if prediction > 0.2: 
                     output_dir_human = os.path.join(self.output_dir, 'human')
                     if not os.path.exists(output_dir_human):
                         os.mkdir(output_dir_human)
-                    cv2.imwrite(os.path.join(output_dir_human, f"{j}_{i}.png"), img)
+                    cv2.imwrite(os.path.join(output_dir_human, f"{j}_{i}.png"), combined_img)
                 else:
                     output_dir_non_human = os.path.join(self.output_dir, 'non_human')
                     if not os.path.exists(output_dir_non_human):
                         os.mkdir(output_dir_non_human)
-                    cv2.imwrite(os.path.join(output_dir_non_human, f"{j}_{i}.png"), img)
+                    cv2.imwrite(os.path.join(output_dir_non_human, f"{j}_{i}.png"), combined_img)
 
-# Example Usage
-# classifier = ImageClassifier()
-# classifier.classify_images()
+
